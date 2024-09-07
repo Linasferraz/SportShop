bbfb02221593ad2c7fff4a7d0fef06b6901475c6
Milena <milena.cod@gmail.com>
2024-09-06 21:51:00 -0300

    adicionando produtos Natação

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -3,9 +3,23 @@ import '../css/global.css';
 
 const Produtos = () => {
   return (
+    <>
     <div className="produtos">
       <h1>Nossos Produtos</h1>
     </div>
+    <h2>Natação</h2>
+    <div className='container-natacao'>
+      <img src="../images/oculos_natacao.png" alt="" />
+      <span>Òculos de Natação</span>
+      <span>R$60,00</span>
+    </div>
+
+    <div className='container-natacao'>
+      <img src="../images/prancha_natacao.png" alt="" />
+      <span>Prancha de Natação</span>
+      <span>R$90,00</span>
+    </div>
+    </>
   );
 };
 
